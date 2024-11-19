12be3291eec526ab1933db2775885a9c73788e25
MartinNoh <ehdrud1129@naver.com>
2024-11-19 21:39:22 +0900

    first commit
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

---
+++ src/test/java/me/donggyeong/ingester/IngesterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.donggyeong.ingester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
