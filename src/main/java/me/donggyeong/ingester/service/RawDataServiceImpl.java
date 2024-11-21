ae94191ef8b7b17d57fef8789e4f83c0f11f010a
MartinNoh <ehdrud1129@naver.com>
2024-11-21 21:46:56 +0900

    Add transactional annotation to RawDataServiceImpl
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

--- src/main/java/me/donggyeong/ingester/service/RawDataServiceImpl.java
+++ src/main/java/me/donggyeong/ingester/service/RawDataServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import me.donggyeong.ingester.domain.RawData;
@@ -13,12 +14,14 @@ import me.donggyeong.ingester.dto.RawDataResponse;
 import me.donggyeong.ingester.repository.RawDataRepository;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class RawDataServiceImpl implements RawDataService {
 
 	private final RawDataRepository rawDataRepository;
 
 	@Override
+	@Transactional
 	public RawDataResponse createRawData(RawDataRequest rawDataRequest) {
 		RawData rawData = rawDataRequest.toEntity();
 		rawData = rawDataRepository.save(rawData);
