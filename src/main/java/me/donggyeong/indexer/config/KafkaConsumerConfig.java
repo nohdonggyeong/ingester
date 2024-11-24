379038d6abe5a705c748e32a0ee63d03663f10bb
MartinNoh <ehdrud1129@naver.com>
2024-11-24 20:41:02 +0900

    Modify source data request type from string to object
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

--- src/main/java/me/donggyeong/indexer/config/KafkaConsumerConfig.java
+++ src/main/java/me/donggyeong/indexer/config/KafkaConsumerConfig.java
@@ -5,28 +5,56 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+import me.donggyeong.indexer.dto.SourceDataRequest;
+
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapAddress;
+
+	@Value("${spring.kafka.consumer.group-id}")
+	private String groupId;
+
 	@Bean
-	public ConsumerFactory<String, Object> consumerFactory() {
-		Map<String, Object> config = new HashMap<>();
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
-		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		return new DefaultKafkaConsumerFactory<>(config);
+	public ConsumerFactory<String, SourceDataRequest> pushEntityConsumerFactory() {
+		JsonDeserializer<SourceDataRequest> deserializer = gcmPushEntityJsonDeserializer();
+		return new DefaultKafkaConsumerFactory<>(
+			consumerFactoryConfig(deserializer),
+			new StringDeserializer(),
+			deserializer);
+	}
+
+	private Map<String, Object> consumerFactoryConfig(JsonDeserializer<SourceDataRequest> deserializer) {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+		return props;
+	}
+
+	private JsonDeserializer<SourceDataRequest> gcmPushEntityJsonDeserializer() {
+		JsonDeserializer<SourceDataRequest> deserializer = new JsonDeserializer<>(SourceDataRequest.class);
+		deserializer.setRemoveTypeHeaders(false);
+		deserializer.setUseTypeMapperForKey(true);
+		deserializer.addTrustedPackages("*");
+		return deserializer;
 	}
 
 	@Bean
-	public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(consumerFactory());
+	public ConcurrentKafkaListenerContainerFactory<String, SourceDataRequest> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, SourceDataRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(pushEntityConsumerFactory());
 		return factory;
 	}
 }
--- src/main/java/me/donggyeong/indexer/service/KafkaConsumerService.java
+++ src/main/java/me/donggyeong/indexer/service/KafkaConsumerService.java
@@ -1,12 +1,20 @@
 package me.donggyeong.indexer.service;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+import me.donggyeong.indexer.dto.SourceDataRequest;
+
 @Service
+@Slf4j
 public class KafkaConsumerService {
-	@KafkaListener(topics = "topic", groupId = "group_1")
-	public void listener(Object data) {
-		System.out.println(data);
+	@KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
+	public void consume(@Payload SourceDataRequest message, @Headers MessageHeaders messageHeaders) {
+		log.info(message.getAction().toString());
+		log.info(message.getDocument().toString());
 	}
 }
